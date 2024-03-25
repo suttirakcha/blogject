a51d585d2973e3bfa5dc7c0dd8ee662ff841e2fd
MarkSuttirak <suttirak.ch@zaviago.com>
2024-03-25 16:41:29 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>HELLO WORLD</h1>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
